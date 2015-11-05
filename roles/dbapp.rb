4049e2aa844a242aa949b3885faa765240ac1621
Tim Smith <tsmith84@gmail.com>
2015-11-05 11:23:46 -0800

    Delete all content and update the readme

--- roles/dbapp.rb
+++
@@ -1,7 +0,0 @@
-name "dbapp"
-description "dbapp front end application server."
-run_list(
-  "recipe[mysql::client]",
-  "recipe[application]",
-  "recipe[dbapp::status]"
-)
--- roles/dbapp_database_master.rb
+++
@@ -1,5 +0,0 @@
-name "dbapp_database_master"
-description "Database master for the dbapp application."
-run_list(
-  "recipe[database::master]"
-)
